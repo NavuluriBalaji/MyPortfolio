c2d7f99bbf3cd87d0fab92085b2573e4ee293def
Balaji Navuluri <117987490+NavuluriBalaji@users.noreply.github.com>
2024-10-15 09:34:00 +0530

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -41,5 +41,127 @@ let projects = {
             "assets/img/portfolio/app-3.jpg",
             "assets/img/portfolio/product-1.jpg",
           ]
-        }
+        },
+
+      "MachineLearning": {
+        // DETAILS SHOWN IN MAIN PAGE.
+          "TITLE": "Machine Learning Projects",
+          "DESCRIPTION": "Pattern and Anomaly Detection.",
+          "CATEGORY": "ml",    // shown in main page, shorter to make it easy for filtering (ml).
+          // category can be  "ml", "genai", "designs", "ds"
+          "IMAGE_URL": "assets/img/AnomalyVoice/c1.jpg",
+          
+          // DETAILS SHOWN IN PROJECT PAGE.
+          "FULL_CATEGORY": "Machine Learning",   // shown in project page, longer and descriptive (Machine Learning).
+          "CLIENT": "Pattern and Anomaly Detection",
+          "PROJECT_URL": "https://github.com/NavuluriBalaji/Anomaly-Detection-in-Voice-Conversations/",
+          "FULL_TITLE": "Anamoly Detection in Voice Conversations.",
+          "FULL_DESCRIPTION": "Anomaly detection in voice conversations involve detection of unusual patterns, behavioral patterns and voice noise, anomaly points using statistical methods in an audio that is conversation between multiple speakers.Anomaly detection in voice conversations involve detection of unusual patterns, behavioral patterns and voice noise, anomaly points using statistical methods in an audio that is conversation between multiple speakers.",
+          "IMAGES": [
+            "assets/img/portfolio/app-1.jpg",
+            "assets/img/portfolio/app-2.jpg",
+            "assets/img/portfolio/app-3.jpg",
+            "assets/img/portfolio/product-1.jpg",
+          ]
+      },    
+
+      "DeepLearning": {
+        // DETAILS SHOWN IN MAIN PAGE.
+          "TITLE": "Deep Learning Projects",
+          "DESCRIPTION": "Deep Learning",
+          "CATEGORY": "ml",    // shown in main page, shorter to make it easy for filtering (ml).
+          // category can be  "ml", "genai", "designs", "ds"
+          "IMAGE_URL": "assets/img/dl/c2.jpg",
+          
+          // DETAILS SHOWN IN PROJECT PAGE.
+          "FULL_CATEGORY": "Deep Learning",   // shown in project page, longer and descriptive (Machine Learning).
+          "CLIENT": "Deep Learning",
+          "PROJECT_URL": "https://github.com/NavuluriBalaji/Design-and-Evaluation-of-a-Deep-Learning-Model-for-Counting-Objects-in-Satellite-Images",
+          "FULL_TITLE": "Design and Evaluation of a Deep Learning Model for Counting Objects in Satellite Images.",
+          "FULL_DESCRIPTION": "TThis study proposes the design and evaluation of a deep learning model using YOLOv8, an advanced object detection algorithm, for object detection and counting in satellite images. The model will leverage techniques like Laplacian of Gaussian (LoG), Difference of Gaussians (DoG), and Determinant of Hessian (DoH) for pre-processing and feature extractionion in voice conversations involve detection of unusual patterns, behavioral patterns and voice noise, anomaly points using statistical methods in an audio that is conversation between multiple speakers.Anomaly detection in voice conversations involve detection of unusual patterns, behavioral patterns and voice noise, anomaly points using statistical methods in an audio that is conversation between multiple speakers.",
+          "IMAGES": [
+            "assets/img/portfolio/app-1.jpg",
+            "assets/img/portfolio/app-2.jpg",
+            "assets/img/portfolio/app-3.jpg",
+            "assets/img/portfolio/product-1.jpg",
+          ]
+      }, 
+
+      
+      "AISR": {
+        // DETAILS SHOWN IN MAIN PAGE.
+          "TITLE": "Machine Learning Projects",
+          "DESCRIPTION": "AISR",
+          "CATEGORY": "ml",    // shown in main page, shorter to make it easy for filtering (ml).
+          // category can be  "ml", "genai", "designs", "ds"
+          "IMAGE_URL": "assets/img/c4.jpg",
+          
+          // DETAILS SHOWN IN PROJECT PAGE.
+          "FULL_CATEGORY": "Machine Learning",   // shown in project page, longer and descriptive (Machine Learning).
+          "CLIENT": "AISR",
+          "PROJECT_URL": "https://github.com/NavuluriBalaji/Pre-Term-Birth-Prediction",
+          "FULL_TITLE": "Pre-Term Birth Prediction.",
+          "FULL_DESCRIPTION": "Preterm Birth is defined as Premature birth that is live birth that occurs before 37 weeks of pregnancy.Predicting preterm birth can help pregnant women to take proactive measures to avoid disabilities and death before baby born",
+          "IMAGES": [
+            "assets/img/portfolio/app-1.jpg",
+            "assets/img/portfolio/app-2.jpg",
+            "assets/img/portfolio/app-3.jpg",
+            "assets/img/portfolio/product-1.jpg",
+          ]
+      }, 
+
+
+
+      
+      "GenerativeAI": {
+        // DETAILS SHOWN IN MAIN PAGE.
+          "TITLE": "Generative AI",
+          "DESCRIPTION": "Applicant Tracking System ",
+          "CATEGORY": "genai",    // shown in main page, shorter to make it easy for filtering (ml).
+          // category can be  "ml", "genai", "designs", "ds"
+          "IMAGE_URL": "assets/img/c5.jpg",
+          
+          // DETAILS SHOWN IN PROJECT PAGE.
+          "FULL_CATEGORY": "Generative AI",   // shown in project page, longer and descriptive (Machine Learning).
+          "CLIENT": "Artificial Intelligence",
+          "PROJECT_URL": "https://github.com/NavuluriBalaji/Anomaly-Detection-in-Voice-Conversations/",
+          "FULL_TITLE": "ATS Resume Analysis using GEN-AI.",
+          "FULL_DESCRIPTION": " The ATS Resume Scanner is a tool designed to help job seekers optimize their resumes for specific job descriptions. By leveraging the Gemini 1.5 Flash model, this tool compares the content of a resume with the provided job description and calculates the percentage match",
+          "IMAGES": [
+            "assets/img/portfolio/app-1.jpg",
+            "assets/img/portfolio/app-2.jpg",
+            "assets/img/portfolio/app-3.jpg",
+            "assets/img/portfolio/product-1.jpg",
+          ]
+      }, 
+
+
+
+
+      
+      "MachineLearning": {
+        // DETAILS SHOWN IN MAIN PAGE.
+          "TITLE": "Machine Learning Projects",
+          "DESCRIPTION": "Pattern and Anomaly Detection.",
+          "CATEGORY": "ml",    // shown in main page, shorter to make it easy for filtering (ml).
+          // category can be  "ml", "genai", "designs", "ds"
+          "IMAGE_URL": "assets/img/AnomalyVoice/c1.jpg",
+          
+          // DETAILS SHOWN IN PROJECT PAGE.
+          "FULL_CATEGORY": "Machine Learning",   // shown in project page, longer and descriptive (Machine Learning).
+          "CLIENT": "Pattern and Anomaly Detection",
+          "PROJECT_URL": "https://github.com/NavuluriBalaji/Anomaly-Detection-in-Voice-Conversations/",
+          "FULL_TITLE": "Anamoly Detection in Voice Conversations.",
+          "FULL_DESCRIPTION": "Anomaly detection in voice conversations involve detection of unusual patterns, behavioral patterns and voice noise, anomaly points using statistical methods in an audio that is conversation between multiple speakers.Anomaly detection in voice conversations involve detection of unusual patterns, behavioral patterns and voice noise, anomaly points using statistical methods in an audio that is conversation between multiple speakers.",
+          "IMAGES": [
+            "assets/img/portfolio/app-1.jpg",
+            "assets/img/portfolio/app-2.jpg",
+            "assets/img/portfolio/app-3.jpg",
+            "assets/img/portfolio/product-1.jpg",
+          ]
+      }, 
+
+
+
+
 }
